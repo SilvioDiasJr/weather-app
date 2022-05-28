1b8433ef13de667fe8eda5a296492e419891f8ab
Silvio <silviodiasjr@outlook.com>
2022-05-28 00:35:30 -0300

    chore: adding EnvironmentPlugin

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 import tsconfigPaths from 'vite-tsconfig-paths'
+import EnvironmentPlugin from 'vite-plugin-environment'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA({}), tsconfigPaths()]
+  plugins: [
+    react(),
+    VitePWA({}),
+    tsconfigPaths(),
+    EnvironmentPlugin(['API_KEY_OPEN_WEATHER', 'API_BASE_URL'], {
+      loadEnvFiles: true
+    })
+  ]
 })

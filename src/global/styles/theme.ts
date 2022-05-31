2f301ca260abe8d15ea340fd1413448a2d6b3886
Silvio <silviodiasjr@outlook.com>
2022-05-30 23:56:13 -0300

    feat: adding new colors

--- src/global/styles/theme.ts
+++ src/global/styles/theme.ts
@@ -1,6 +1,14 @@
 export default {
   COLORS: {
     brand: {
+      button_background: '#6E707A',
+
+      border: '#616475',
+      outline: '#E7E7EB',
+
+      blue: '#3C47E9',
+      yellow: '#FFEC65',
+
       background: '#100E1D'
     },
     shape: {
@@ -8,7 +16,9 @@ export default {
     },
     text: {
       heading: '#E7E7EB',
-      body: '#A09FB1'
+      body: '#A09FB1',
+      info: '#88869D',
+      placeholder: '#616475'
     }
   }
 }

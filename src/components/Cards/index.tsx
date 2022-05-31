84cecda387a75d364b2592586f5eac1a263fe719
Silvio <silviodiasjr@outlook.com>
2022-05-31 00:04:50 -0300

    feat: creating export cards

---
+++ src/components/Cards/index.tsx
@@ -0,0 +1,13 @@
+import { WeatherForecast } from './WeatherForecast'
+import { WindStatus } from './TodaysHighlight/WindStatus'
+import { Humidity } from './TodaysHighlight/Humidity'
+import { Visibility } from './TodaysHighlight/Visibility'
+import { Pressure } from './TodaysHighlight/Pressure'
+
+export const Cards = {
+  WeatherForecast,
+  WindStatus,
+  Humidity,
+  Visibility,
+  Pressure
+}

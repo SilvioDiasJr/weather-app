137b18112bf8977a8575dec02890ddd33f4278c8
Silvio <silviodiasjr@outlook.com>
2022-05-31 00:08:08 -0300

    feat: adding Home page

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,15 @@ import React from 'react'
 
 import { ThemeProvider } from 'styled-components'
 
+import { Home } from '@pages/Home'
+
 import theme from '@global/styles/theme'
 import { GlobalStyle } from '@global/styles/global'
 
 export const App: React.FC = () => {
   return (
     <ThemeProvider theme={theme}>
-      <h1>Hello world</h1>
+      <Home />
 
       <GlobalStyle />
     </ThemeProvider>
